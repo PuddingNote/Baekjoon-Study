0c3134125e38bb8712863e029918efcbf4744649
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-04-12 19:07:03 +0900

    추가: 9086번
    1. 9086번 풀이 완료

---
+++ Baekjoon Study/Baekjoon Study/9086.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	int T;
+	string s;
+
+	cin >> T;
+
+	for (int i = 0; i < T; i++)
+	{
+		cin >> s;
+		cout << s[0] << s[s.length() - 1] << endl;
+	}
+
+	return 0;
+}
