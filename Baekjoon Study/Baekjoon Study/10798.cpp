f714a1d81903be237a96448c753da45b318a1f8c
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-04-17 14:52:14 +0900

    추가: 10798번
    1. 10798번 풀이 완료

---
+++ Baekjoon Study/Baekjoon Study/10798.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char c[5][15] = { '\0' };
+
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> c[i];
+	}
+
+	for (int i = 0; i < 15; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (c[j][i] != '\0')
+			{
+				cout << c[j][i];
+			}
+		}
+	}
+
+	return 0;
+}
