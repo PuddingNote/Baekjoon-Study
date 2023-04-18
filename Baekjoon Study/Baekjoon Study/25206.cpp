a18dc9207590f1640af15b235b1781d6d889f59d
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-04-18 14:06:16 +0900

    추가: 25206번
    1. 25206번 풀이 완료

---
+++ Baekjoon Study/Baekjoon Study/25206.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define SubjectCnt 20
+
+int main()
+{
+	string subject;
+	float inputScore;
+	string grade;
+
+    float sumScore = 0;
+    float totalScore = 0;
+    
+    for (int i = 0; i < SubjectCnt; i++)
+    {
+        cin >> subject >> inputScore >> grade;
+
+        totalScore += inputScore;
+
+        if (grade == "P") totalScore -= inputScore;
+        else if (grade == "A+") sumScore += (inputScore * 4.5);
+        else if (grade == "A0") sumScore += (inputScore * 4.0);
+        else if (grade == "B+") sumScore += (inputScore * 3.5);
+        else if (grade == "B0") sumScore += (inputScore * 3.0);
+        else if (grade == "C+") sumScore += (inputScore * 2.5);
+        else if (grade == "C0") sumScore += (inputScore * 2.0);
+        else if (grade == "D+") sumScore += (inputScore * 1.5);
+        else if (grade == "D0") sumScore += (inputScore * 1.0);
+        else if (grade == "F")  sumScore += (inputScore * 0.0);
+    }
+
+    cout << fixed;
+    cout.precision(6);
+    cout << sumScore / totalScore << endl;
+
+	return 0;
+}
