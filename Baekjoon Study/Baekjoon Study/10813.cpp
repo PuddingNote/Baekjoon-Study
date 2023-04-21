5256ca595a4bb531d3cc60ca1ee89e0bfc1b4dfe
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-04-22 01:35:38 +0900

    추가: 10813번
    1. 10813번 풀이 완료

---
+++ Baekjoon Study/Baekjoon Study/10813.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N, M;
+	int i, j;
+	int temp = 0;
+
+	cin >> N >> M;
+
+	int* arr = new int[N];
+	for (int x = 0; x < N; x++)
+	{
+		arr[x] = x + 1;
+	}
+
+	for (int x = 0; x < M; x++)
+	{
+		cin >> i >> j;
+
+		temp = arr[i - 1];
+		arr[i - 1] = arr[j - 1];
+		arr[j - 1] = temp;
+	}
+
+	for (int x = 0; x < N; x++)
+	{
+		cout << arr[x] << " ";
+	}
+
+	return 0;
+}
