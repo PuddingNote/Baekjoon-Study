8122cfdfd999962bedce8ad28f2e96746d565689
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-05-06 23:31:31 +0900

    추가: 24264번
    1. 24264번 풀이 완료

---
+++ Baekjoon Study/Baekjoon Study/24264.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+//MenOfPassion(A[], n) {
+//    sum < -0;
+//    for i < -1 to n
+//        for j < -1 to n
+//            sum < -sum + A[i] ¡¿ A[j]; # ÄÚµå1
+//            return sum;
+//}
+
+int main()
+{
+	long long N;
+
+	cin >> N;
+
+	cout << N * N << endl << 2;
+
+	return 0;
+}
