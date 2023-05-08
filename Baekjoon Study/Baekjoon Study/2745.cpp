645f67bc284da11c8933c7340ea6347ebab28679
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-05-08 10:24:06 +0900

    추가: 2745번
    1. 2745번 풀이 완료

---
+++ Baekjoon Study/Baekjoon Study/2745.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int b, sum = 0;
+
+	cin >> s >> b;
+
+	for (int i = 0; i < s.size(); i++)	// s.length()
+	{	
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			sum = sum * b + (s[i] - '0');
+		}
+		else
+		{
+			sum = sum * b + (s[i] - 'A' + 10);
+		}
+	}
+	cout << sum << endl;
+
+	return 0;
+}
