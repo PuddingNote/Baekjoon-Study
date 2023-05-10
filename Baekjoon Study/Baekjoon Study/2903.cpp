94a34b533d384c6a37884375e966b10d17a9d9cf
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-05-10 15:45:37 +0900

    추가: 2903번
    1. 2903번 풀이 완료

---
+++ Baekjoon Study/Baekjoon Study/2903.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+	int N;
+	int dot = 2;
+
+	cin >> N;
+	
+	while (N--)
+	{
+		dot += dot - 1;
+	}
+
+	dot *= dot;
+
+	cout << dot << endl;
+
+	return 0;
+}
