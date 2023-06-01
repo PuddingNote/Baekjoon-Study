f54a6bcfa49317457f9ff5e9637c203854ea4289
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-06-01 09:51:41 +0900

    추가: 10814번
    1. 10814번 풀이 완료

---
+++ Baekjoon Study/Baekjoon Study/10814.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+struct Member
+{
+	int index;
+	int age;
+	string name;
+};
+
+bool compare(const Member& m1, const Member& m2)
+{
+	if (m1.age != m2.age)
+	{
+		return m1.age < m2.age;
+	}
+	else
+	{
+		return m1.index < m2.index;
+	}
+}
+
+int main()
+{
+	int N;
+	
+	cin >> N;
+
+	Member* m = new Member[N];
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> m[i].age >> m[i].name;
+		m[i].index = i;
+	}
+
+	sort(m, m + N, compare);
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << m[i].age << " " << m[i].name << '\n';
+	}
+
+	return 0;
+}
