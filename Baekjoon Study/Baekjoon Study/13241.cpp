4ae13860d01c767d15054692c655c921f324563d
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-06-14 21:31:13 +0900

    추가: 13241번
    1. 13241번 풀이 완료

---
+++ Baekjoon Study/Baekjoon Study/13241.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+long long GCD(long long a, long long b)
+{
+    if (a < b) 
+    { 
+        return GCD(a, b % a);
+    }
+
+    if (b == 0) 
+    { 
+        return a; 
+    }
+
+    return GCD(b, a % b);
+}
+
+int main() 
+{
+    long long a, b, gcd;
+
+    cin >> a >> b;
+
+    gcd = GCD(a, b);
+
+    cout << gcd * (a / gcd) * (b / gcd) << '\n';
+
+    return 0;
+}
