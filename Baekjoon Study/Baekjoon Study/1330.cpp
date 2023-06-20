0836a1502cd19c76bac7ab912fcd7b89e2be916a
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-06-21 02:38:01 +0900

    추가: 1330번
    1. 1330번 풀이 완료

---
+++ Baekjoon Study/Baekjoon Study/1330.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, B;
+
+	cin >> A >> B;
+
+	if (A > B)
+	{
+		cout << ">" << '\n';
+	}
+	else if (A < B)
+	{
+		cout << "<" << '\n';
+	}
+	else if (A == B)
+	{
+		cout << "==" << '\n';
+	}
+
+	return 0;
+}
