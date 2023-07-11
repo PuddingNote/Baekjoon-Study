540d0eee0d2d6f6ba2814a1e5916136b2a819e3a
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-07-11 12:46:24 +0900

    수정: 11654번
    1. 11654번 수정 완료

--- Baekjoon Study/Baekjoon Study/11654.cpp
+++ Baekjoon Study/Baekjoon Study/11654.cpp
@@ -3,12 +3,10 @@ using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
 	char a;
 	cin >> a;
 
-	cout << (int)a << "\n";
+	cout << (int)a << '\n';
 
+	return 0;
 }
