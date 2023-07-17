b3dd85fbdf502210e38e59f6b276d9a1c3f2d978
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-07-17 17:17:45 +0900

    수정: 5622번
    1. 5622번 수정 완료

--- Baekjoon Study/Baekjoon Study/5622.cpp
+++ Baekjoon Study/Baekjoon Study/5622.cpp
@@ -15,10 +15,12 @@ int main()
 		count += ((int)s[i] - 65) / 3 + 3;
 
 		if (s[i] == 'S' || s[i] == 'V' || s[i] == 'Y' || s[i] == 'Z') // PQRS부터 4자리라 하나씩 밀림
+		{
 			count--;
+		}
 	}
 
-	cout << count;
+	cout << count << '\n';
 
 	return 0;
 }
