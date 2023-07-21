5326a805be1a7dfa4e60e83adf2aa67d5584f6da
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-07-21 18:11:04 +0900

    추가: 3003번
    1. 3003번 풀이 완료

---
+++ Baekjoon Study/Baekjoon Study/3003.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int save[6] = { 1,1,2,2,2,8 };
+	int input[6] = { 0, };
+
+	for (int i = 0; i < 6; i++)
+	{
+		cin >> input[i];
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (save[i] == input[i])
+		{
+			cout << "0";
+		}
+		else
+		{
+			cout << save[i] - input[i];
+		}
+		cout << " ";
+	}
+	cout << '\n';
+
+	return 0;
+}
