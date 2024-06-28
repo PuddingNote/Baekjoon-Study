0e9568972999d060040b53e5433cdbb9f9144f9d
Pudding <gkscksgma14@gmail.com>
2024-06-28 12:38:03 +0900

    수정: 2501번
    1. 2501번 수정 완료

--- Baekjoon Study/Baekjoon Study/2501.cpp
+++ Baekjoon Study/Baekjoon Study/2501.cpp
@@ -1,23 +1,66 @@
+// return 0;이 한개
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main()
 {
-	int N, K;
-	vector<int> v;
+    int N, K;
+    int result = 0;
 
-	cin >> N >> K;
+    cin >> N >> K;
 
-	for (int i = 1; i <= N; i++)
-	{
-		if (N % i == 0)
-		{
-			v.push_back(i);
-		}
-	}
+    for (int i = 1; i <= N; i++)
+    {
+        if (N % i == 0)
+        {
+            K--;
 
-	cout << v[K - 1] << endl;
+            if (K == 0)
+            {
+                result = i;
+                break;
+            }
+        }
+    }
 
-	return 0;
+    if (K != 0)
+    {
+        cout << 0 << '\n';
+    }
+    else
+    {
+        cout << result << '\n';
+    }
+
+
+    return 0;
+}
+
+// return 0;이 여러개
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, K;
+
+    cin >> N >> K;
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (N % i == 0)
+        {
+			K--;
+
+            if (K == 0)
+            {
+                cout << i << '\n';
+                return 0;
+            }
+        }
+    }
+
+    cout << 0 << '\n';
+
+    return 0;
 }
