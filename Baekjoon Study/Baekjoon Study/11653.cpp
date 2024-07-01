51963ed01e10ca0d35ece16fe286e8708cf3465d
Pudding <gkscksgma14@gmail.com>
2024-07-02 00:49:30 +0900

    수정: 11653번
    1. 11653번 수정 완료

--- Baekjoon Study/Baekjoon Study/11653.cpp
+++ Baekjoon Study/Baekjoon Study/11653.cpp
@@ -1,3 +1,4 @@
+// #1
 #include <iostream>
 using namespace std;
 
@@ -13,10 +14,43 @@ int main()
 		if (N % so == 0)
 		{
 			N = N / so;
-			cout << so << endl;
+			cout << so << '\n';
 		}
 		else so++;
 	}
 
 	return 0;
+}
+
+// #2
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    while (N % 2 == 0)
+    {
+        cout << 2 << '\n';
+        N /= 2;
+    }
+
+    for (int so = 3; so <= sqrt(N); so += 2)
+    {
+        while (N % so == 0)
+        {
+            cout << so << '\n';
+            N /= so;
+        }
+    }
+
+    if (N > 2)
+    {
+        cout << N << '\n';
+    }
+
+    return 0;
 }
