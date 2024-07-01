07ddb6dd0c6e34b28aba36ea46fe2da663e07b67
Pudding <gkscksgma14@gmail.com>
2024-07-01 21:27:21 +0530

    수정: 27323번
    1. 27323번 수정 완료

--- Baekjoon Study/Baekjoon Study/27323.cpp
+++ Baekjoon Study/Baekjoon Study/27323.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
 	int A, B;
-	int result;
-
 	cin >> A >> B;
 
-	result = A * B;
-
-	cout << result << endl;
+	cout << A * B << '\n';
 
 	return 0;
 }
