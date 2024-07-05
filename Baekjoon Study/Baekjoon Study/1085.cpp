b615e830591a1a7bf37d9796d1130f7cecda2b43
Pudding <gkscksgma14@gmail.com>
2024-07-05 11:43:51 +0900

    수정: 1085번
    1. 1085번 수정 완료

--- Baekjoon Study/Baekjoon Study/1085.cpp
+++ Baekjoon Study/Baekjoon Study/1085.cpp
@@ -1,14 +1,34 @@
+// #1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, w, h;         // (x,y), (w,h)
+    cin >> x >> y >> w >> h;
+
+    int minDistance = x;
+    if (y < minDistance) minDistance = y;
+    if (w - x < minDistance) minDistance = w - x;
+    if (h - y < minDistance) minDistance = h - y;
+
+    cout << minDistance << '\n';
+
+    return 0;
+}
+
+// #2
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 int main()
 {
-	int x, y, w, h;
+	int x, y, w, h;         // (x,y), (w,h)
 
 	cin >> x >> y >> w >> h;
 
-	cout << min(min(x, y), min(w - x, h - y));
+	cout << min(min(x, y), min(w - x, h - y)) << '\n';
 
 	return 0;
 }
