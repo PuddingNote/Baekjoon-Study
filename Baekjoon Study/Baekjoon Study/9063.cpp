d87cf1b58b8efae38311c04460bd338beda13cfe
Pudding <gkscksgma14@gmail.com>
2024-07-08 14:09:30 +0900

    수정: 9063번
    1. 9063번 수정 완료

--- Baekjoon Study/Baekjoon Study/9063.cpp
+++ Baekjoon Study/Baekjoon Study/9063.cpp
@@ -1,3 +1,4 @@
+// #1
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -20,7 +21,71 @@ int main()
 
 	result = (x[N - 1] - x[0]) * (y[N - 1] - y[0]);
 
-	cout << result << endl;
+	cout << result << '\n';
+
+	return 0;
+}
+
+// #2
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int N, result;
+	cin >> N;
+
+	vector<int> x(N), y(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> x[i] >> y[i];
+	}
+
+	sort(x.begin(), x.end());
+	sort(y.begin(), y.end());
+
+	result = (x[N - 1] - x[0]) * (y[N - 1] - y[0]);
+
+	cout << result << '\n';
+
+	return 0;
+}
+
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int N, result;
+	cin >> N;
+
+	vector<int> x(N), y(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> x[i] >> y[i];
+	}
+
+	auto minmax_x = minmax_element(x.begin(), x.end());
+	auto minmax_y = minmax_element(y.begin(), y.end());
+
+	result = (*minmax_x.second - *minmax_x.first) * (*minmax_y.second - *minmax_y.first);
+
+	/*auto min_x = min_element(x.begin(), x.end());
+	auto max_x = max_element(x.begin(), x.end());
+	auto min_y = min_element(y.begin(), y.end());
+	auto max_y = max_element(y.begin(), y.end());*/
+
+	//result = (*max_x - *min_x) * (*max_y - *min_y);
+
+	cout << result << '\n';
 
 	return 0;
 }
