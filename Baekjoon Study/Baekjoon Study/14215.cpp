e4cf26e219b60e4b47096fbaaa03c4490990b15c
Pudding <gkscksgma14@gmail.com>
2024-07-11 12:33:16 +0900

    수정: 14215번
    1. 14215번 수정 완료

--- Baekjoon Study/Baekjoon Study/14215.cpp
+++ Baekjoon Study/Baekjoon Study/14215.cpp
@@ -8,18 +8,21 @@ int main()
 
 	cin >> a >> b >> c;
 
+	// a, b, c 오름차순 정렬
 	if (a > b) swap(a, b);
 	if (a > c) swap(a, c);
 	if (b > c) swap(b, c);
 
 	if (a + b > c)
 	{
-		cout << a + b + c;
+		cout << a + b + c << '\n';
 	}
 	else
 	{
-		cout << (a + b) * 2 - 1;
+		cout << (a + b) * 2 - 1 << '\n';
 	}
+
+	// cout << ((a + b > c) ? (a + b + c) : (a + b) * 2 - 1) << '\n';
 
 	return 0;
 }
