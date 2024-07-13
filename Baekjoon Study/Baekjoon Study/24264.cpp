14b79ca8c96d1cc11b1f6798a083a0d514ec560b
Pudding <gkscksgma14@gmail.com>
2024-07-14 00:34:36 +0900

    수정: 24264번
    1. 24264번 수정 완료

--- Baekjoon Study/Baekjoon Study/24264.cpp
+++ Baekjoon Study/Baekjoon Study/24264.cpp
@@ -1,22 +1,13 @@
 #include <iostream>
-
 using namespace std;
-
-//MenOfPassion(A[], n) {
-//    sum < -0;
-//    for i < -1 to n
-//        for j < -1 to n
-//            sum < -sum + A[i] ¡¿ A[j]; # ÄÚµå1
-//            return sum;
-//}
 
 int main()
 {
-	long long N;
+	long N;
 
 	cin >> N;
 
-	cout << N * N << endl << 2;
+	cout << N * N << '\n' << 2 << '\n';
 
 	return 0;
 }
