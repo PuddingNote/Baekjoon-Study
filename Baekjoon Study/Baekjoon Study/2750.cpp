45abb668696522d7e228b5e1f4f030540d06029a
Pudding <gkscksgma14@gmail.com>
2024-07-25 11:20:52 +0900

    수정: 2750번
    1. 2750번 수정 완료

--- Baekjoon Study/Baekjoon Study/2750.cpp
+++ Baekjoon Study/Baekjoon Study/2750.cpp
@@ -1,35 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
 	int N;
-	int arr[1000];
-	int temp = 0;
-
 	cin >> N;
 
+	vector<int> v(N);
 	for (int i = 0; i < N; i++)
 	{
-		cin >> arr[i];
+		cin >> v[i];
 	}
+
+	sort(v.begin(), v.end());
 
 	for (int i = 0; i < N; i++)
 	{
-		for (int j = i + 1; j < N; j++)
-		{
-			if (arr[i] > arr[j])
-			{
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-		}
-	}
-
-	for (int i = 0; i < N; i++)
-	{
-		cout << arr[i] << endl;
+		cout << v[i] << '\n';
 	}
 
 	return 0;
