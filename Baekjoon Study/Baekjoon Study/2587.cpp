182f7e04ead349931d4c7580ecd2b7925e4f3f8f
Pudding <gkscksgma14@gmail.com>
2024-07-26 11:33:16 +0900

    수정: 2587번
    1. 2587번 수정 완료

--- Baekjoon Study/Baekjoon Study/2587.cpp
+++ Baekjoon Study/Baekjoon Study/2587.cpp
@@ -1,34 +1,69 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-
-#define LENGTH 5
 
 int main()
 {
-	int arr[LENGTH];
-	int sum = 0, avg = 0;
+	vector<int> v(5);
+	int input = 0, sum = 0, avg = 0, middle = 0;
 
-	for (int i = 0; i < LENGTH; i++)
+	for (int i = 0; i < v.size(); i++)
 	{
-		cin >> arr[i];
-		sum += arr[i];
-	}
+		cin >> input;
 
-	for (int i = 1; i < LENGTH; i++)	// »ðÀÔÁ¤·Ä
-	{
-		int temp = arr[i];
-		int prev = i - 1;
-		while ((prev >= 0) && (arr[prev] > temp)) 
+		if (input > 0 && input < 100 && input % 10 == 0)
 		{
-			arr[prev + 1] = arr[prev];
-			prev--;
+			v[i] = input;
+			sum += v[i];
 		}
-		arr[prev + 1] = temp;
+		else
+		{
+			i--;
+		}
 	}
 
-	avg = sum / LENGTH;
+	sort(v.begin(), v.end());
 
-	cout << avg << '\n' << arr[2] << '\n';
+	avg = sum / v.size();
+	middle = v[v.size() / 2];
+
+	cout << avg << '\n' << middle << '\n';
 
 	return 0;
 }
+
+//#include <iostream>
+//using namespace std;
+//
+//#define LENGTH 5
+//
+//int main()
+//{
+//	int arr[LENGTH];
+//	int sum = 0, avg = 0;
+//
+//	for (int i = 0; i < LENGTH; i++)
+//	{
+//		cin >> arr[i];
+//		sum += arr[i];
+//	}
+//
+//	for (int i = 1; i < LENGTH; i++)	// »ðÀÔÁ¤·Ä
+//	{
+//		int temp = arr[i];
+//		int prev = i - 1;
+//		while ((prev >= 0) && (arr[prev] > temp)) 
+//		{
+//			arr[prev + 1] = arr[prev];
+//			prev--;
+//		}
+//		arr[prev + 1] = temp;
+//	}
+//
+//	avg = sum / LENGTH;
+//
+//	cout << avg << '\n' << arr[2] << '\n';
+//
+//	return 0;
+//}
