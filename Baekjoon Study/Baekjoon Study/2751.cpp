de73d0584c73e1dae4cb48c65118560d306483fd
Pudding <gkscksgma14@gmail.com>
2024-07-28 13:07:44 +0900

    수정: 2751번
    1. 2751번 수정 완료

--- Baekjoon Study/Baekjoon Study/2751.cpp
+++ Baekjoon Study/Baekjoon Study/2751.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int arr[1000000];
-
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
 	int N;
 	cin >> N;
 
+	vector<int> v(N);
+
 	for (int i = 0; i < N; i++)
 	{
-		cin >> arr[i];
+		cin >> v[i];
 	}
 
-	sort(arr, arr + N);
+	sort(v.begin(), v.end());
 
 	for (int i = 0; i < N; i++)
 	{
-		cout << arr[i] << '\n';
+		cout << v[i] << '\n';
 	}
 
 	return 0;
