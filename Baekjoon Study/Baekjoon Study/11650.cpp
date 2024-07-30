e665d97aa2f0a9cbe7f51f80226c71f7fe346928
Pudding <gkscksgma14@gmail.com>
2024-07-31 00:57:49 +0900

    수정: 11650번
    1. 11650번 수정 완료

--- Baekjoon Study/Baekjoon Study/11650.cpp
+++ Baekjoon Study/Baekjoon Study/11650.cpp
@@ -10,17 +10,13 @@ struct Point
 
 bool comparePoints(Point p1, Point p2) 
 {
-	if (p1.x > p2.x)
+	if (p1.x != p2.x) 
 	{
-		return false;
-	}
-	else if (p1.x == p2.x && p1.y > p2.y)
-	{
-		return false;
+		return p1.x < p2.x;
 	}
 	else 
 	{
-		return true;
+		return p1.y < p2.y;
 	}
 }
 
@@ -29,21 +25,17 @@ int main()
 	int N;
 
 	cin >> N;
-	//Point* p = new Point[N];
 	vector<Point> v(N);
 
 	for (int i = 0; i < N; i++)
 	{
-		//cin >> p[i].x >> p[i].y;
 		cin >> v[i].x >> v[i].y;
 	}
 
-	//sort(p, p + N, comparePoints);
 	sort(v.begin(), v.end(), comparePoints);
 
 	for (int i = 0; i < N; i++)
 	{
-		//cout << p[i].x << " " << p[i].y << '\n';	// endl써서 시간초과 떴었음
 		cout << v[i].x << " " << v[i].y << '\n'; 
 	}
 
