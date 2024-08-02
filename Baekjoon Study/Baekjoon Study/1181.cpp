2ff3402e33100323dd9d5f0502760925c71078dd
Pudding <gkscksgma14@gmail.com>
2024-08-02 11:28:45 +0900

    수정: 1181번
    1. 1181번 수정 완료

--- Baekjoon Study/Baekjoon Study/1181.cpp
+++ Baekjoon Study/Baekjoon Study/1181.cpp
@@ -19,20 +19,21 @@ bool compare(string a, string b)
 int main()
 {
 	int N;
-	string s;
-	vector<string> v;
-
 	cin >> N;
-	
+
+	vector<string> v(N);
 	for (int i = 0; i < N; i++)
 	{
-		cin >> s;
-		if (find(v.begin(), v.end(), s) == v.end())
-		{	// 같은거 있으면 추가X
-			v.push_back(s);
-		}
+		cin >> v[i];
 	}
 
+	// unique를 쓰기위한 정렬
+	sort(v.begin(), v.end());
+
+	// 연속된 중복 요소들을 골라낸 후 제거
+	v.erase(unique(v.begin(), v.end()), v.end());
+
+	// 문제 조건에 맞게 다시 정렬
 	sort(v.begin(), v.end(), compare);
 
 	for (int i = 0; i < v.size(); i++)
