7ceb5dc61d8cf0becb4156a69c3a21b4ed6dac23
Pudding <gkscksgma14@gmail.com>
2024-08-04 12:29:07 +0900

    수정: 18870번
    1. 18870번 수정 완료

--- Baekjoon Study/Baekjoon Study/18870.cpp
+++ Baekjoon Study/Baekjoon Study/18870.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-	int N, input;
+	int N;
 	cin >> N;
 
-	vector<int> x1;
-	vector<int> x2;
+	vector<int> x1(N);
+	vector<int> x2(N);
 
+	int input;
 	for (int i = 0; i < N; i++)
 	{
 		cin >> input;
-		x1.push_back(input);
-		x2.push_back(input);
+
+		x1[i] = input;
+		x2[i] = input;
 	}
-	sort(x2.begin(), x2.end());	// 정렬
+
+	sort(x2.begin(), x2.end());	// unique함수 사용을 위해 정렬
 
 	x2.erase(unique(x2.begin(), x2.end()), x2.end()); // 중복된 원소 제거
 
 	for (int i = 0; i < N; i++) 
 	{
-		// 찾으려는 key 값보다 같거나 큰 숫자가 배열 몇 번째에서 처음 등장하는지 찾기 위함
+		// 찾으려는 key 값보다 크거나 같은 숫자가 배열 몇 번째에 처음 등장하는지 찾는 함수
 		cout << lower_bound(x2.begin(), x2.end(), x1[i]) - x2.begin() << " ";
 	}
 
