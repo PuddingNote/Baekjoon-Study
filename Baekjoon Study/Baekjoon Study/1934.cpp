655e938d62125c47e79cfc1b7a0aead5175f7b1c
Pudding <gkscksgma14@gmail.com>
2024-08-15 20:32:18 +0900

    수정: 1934번
    1. 1934번 수정 완료

--- Baekjoon Study/Baekjoon Study/1934.cpp
+++ Baekjoon Study/Baekjoon Study/1934.cpp
@@ -1,36 +1,58 @@
 #include <iostream>
-
+//#include <algorithm>
 using namespace std;
 
-int divide(int x, int y)
+// 유클리드 호제법
+// 숫자 A, B가 (A > B) 존재할때 C = A % B
+// 이때 A와 B의 최대공약수는 B와 C의 최대 공약수와 같음
+// 이 법칙을 반복하여 C가 0이 될때의 B값을 구하는 것
+// 최대공약수 (재귀적 방식)
+//int GCD(int A, int B)
+//{
+//    if (B > A)
+//    {
+//        swap(A, B);
+//    }
+//
+//    int temp = A % B;
+//
+//    if (temp == 0)
+//    {
+//        return B;
+//    }
+//    return GCD(B, temp);
+//}
+
+// 최대공약수 (반복적 방식)
+int GCD(int A, int B)
 {
-    if (x % y == 0)
+    while (B != 0)
     {
-        return y;
+        int temp = B;
+        B = A % B;
+        A = temp;
     }
-    else
-    {
-        return divide(y, x % y);
-    }
+    return A;
+}
+
+// 최소공배수
+int LCM(int a, int b)
+{
+    return (a * b) / GCD(a, b);
 }
 
 int main()
 {
     int T;
-    int A, B;
     cin >> T;
 
+    int A, B;
     for (int i = 0; i < T; i++)
     {
         cin >> A >> B;
 
-        if (A >= B)
-        {
-            cout << A * B / divide(A, B) << "\n";
-        }
-        else
-        {
-            cout << A * B / divide(B, A) << "\n";
-        }
+        cout << LCM(A, B) << '\n';
     }
+
+    return 0;
 }
